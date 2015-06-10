93d762a25dfc46077853be8bfaa357ca417878ef
Brad Herring <brad@bherville.com>
2015-06-10 15:11:22 -0400

    Fixed: Typo in require for helper.

--- lib/look_and_feel.rb
+++ lib/look_and_feel.rb
@@ -1,6 +1,6 @@
 require 'look_and_feel/version'
 require 'look_and_feel/engine'
-require 'semantic_id/helper'
+require 'look_and_feel/helper'
 
 ActiveSupport.on_load(:action_view) do
   include LookAndFeel::Helper
